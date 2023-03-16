b2abf29216f17c6c8494fbf6a2758c00abb70f2f
Daniel Lai <dlai@ufl.edu>
2023-03-16 14:36:41 -0400

    Create sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <aside className= "sidebar">
+        <h1>aside</h1>
+      </aside>
+      <section>
+        
+      </section>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
