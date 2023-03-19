bde4ff633f1de894eb5cb905092e67bd647a79b9
Daniel Lai <dlai@ufl.edu>
2023-03-18 22:38:08 -0400

    add chatbar and attributes

--- src/App.js
+++ src/App.js
@@ -7,23 +7,9 @@ function App() {
       <aside className= "sidebar">
         <h1>aside</h1>
       </aside>
-      <section>
-        
+      <section className= "chatbar">
+
       </section>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
